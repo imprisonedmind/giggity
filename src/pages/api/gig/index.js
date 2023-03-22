cd26edc61a9abde1b5ce1d8bd3c0fd098981441a
Luke <luke.stephens@trintel.co.za>
2023-03-22 16:57:28 +0200

    playwright v1

--- src/pages/api/gig/index.js
+++ src/pages/api/gig/index.js
@@ -1,4 +1,4 @@
-import {chromium} from 'playwright-core';
+import {chromium} from "playwright";
 
 export default async function handler(req, res) {
   try {
@@ -7,13 +7,10 @@ export default async function handler(req, res) {
       throw new Error('Not an Instagram link');
     }
 
-
-    const browser = await chromium.launch({
-      headless: true,
-    })
+    const browser = await chromium.launch({headless: true});
 
     const page = await browser.newPage();
-    await page.goto(link, {waitUntil: 'networkidle'});
+    await page.goto(link, {waitUntil: 'networkidle0'});
 
     const imgClass = 'img.x5yr21d.xu96u03.x10l6tqk.x13vifvy.x87ps6o.xh8yej3'
     const descClass = 'h1._aacl._aaco._aacu._aacx._aad7._aade'
