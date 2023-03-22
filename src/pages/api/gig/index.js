51c9cf2992384314bad863a12d1cdef5e7664b0b
Luke <luke.stephens@trintel.co.za>
2023-03-22 16:06:36 +0200

    puppeteer core 10.1.0 and chrome-aws-lambda

--- src/pages/api/gig/index.js
+++ src/pages/api/gig/index.js
@@ -1,4 +1,5 @@
-import { chromium } from 'playwright-core';
+import puppeteer from 'puppeteer-core';
+import chromium from 'chrome-aws-lambda'
 
 export default async function handler(req, res) {
   try {
@@ -7,21 +8,31 @@ export default async function handler(req, res) {
       throw new Error('Not an Instagram link');
     }
 
-    const browser = await chromium.launch({ headless: true });
+    const LOCAL_CHROME_EXECUTABLE = '/Applications/Google Chrome.app/Contents/MacOS/Google Chrome'
+    const executablePath = await chromium.executablePath || LOCAL_CHROME_EXECUTABLE
+
+    const browser = await puppeteer.launch({
+      executablePath,
+      args: chromium.args,
+      headless: true,
+    })
+
     const page = await browser.newPage();
-    await page.goto(link, { waitUntil: 'networkidle' });
+    await page.goto(link, {waitUntil: 'networkidle0'});
 
-    const imgClass = 'img.x5yr21d.xu96u03.x10l6tqk.x13vifvy.x87ps6o.xh8yej3';
-    const descClass = 'h1._aacl._aaco._aacu._aacx._aad7._aade';
+    const imgClass = 'img.x5yr21d.xu96u03.x10l6tqk.x13vifvy.x87ps6o.xh8yej3'
+    const descClass = 'h1._aacl._aaco._aacu._aacx._aad7._aade'
 
-    const imageUrl = await page.$eval(imgClass, (el) => el.getAttribute('src'));
-    const description = await page.$eval(descClass, (el) => el.innerText);
+    const imageUrl = await page.$eval(imgClass, (el) =>
+        el.getAttribute('src'));
+    const description = await page.$eval(descClass, (el) =>
+        el.innerText);
 
     await browser.close();
 
-    res.status(200).json({ imageUrl, description });
+    res.status(200).json({imageUrl, description});
   } catch (error) {
     console.error(error);
-    res.status(400).json({ error: error.message });
+    res.status(400).json({error: error.message});
   }
 }
