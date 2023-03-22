a6c9546c7671893955d05780b7ed24f6c81efc8b
Luke <luke.stephens@trintel.co.za>
2023-03-22 15:29:08 +0200

    test conversion to playwright

--- src/pages/api/gig/index.js
+++ src/pages/api/gig/index.js
@@ -1,5 +1,4 @@
-import chrome from 'chrome-aws-lambda';
-import puppeteer from 'puppeteer-core';
+import { chromium } from 'playwright';
 
 export default async function handler(req, res) {
   try {
@@ -8,30 +7,21 @@ export default async function handler(req, res) {
       throw new Error('Not an Instagram link');
     }
 
-    const options = {
-      args: chrome.args,
-      executablePath: await chrome.executablePath,
-      headless: true
-    }
-
-    const browser = await puppeteer.launch(options);
-
+    const browser = await chromium.launch({ headless: true });
     const page = await browser.newPage();
-    await page.goto(link, {waitUntil: 'networkidle0'});
+    await page.goto(link, { waitUntil: 'networkidle' });
 
-    const imgClass = 'img.x5yr21d.xu96u03.x10l6tqk.x13vifvy.x87ps6o.xh8yej3'
-    const descClass = 'h1._aacl._aaco._aacu._aacx._aad7._aade'
+    const imgClass = 'img.x5yr21d.xu96u03.x10l6tqk.x13vifvy.x87ps6o.xh8yej3';
+    const descClass = 'h1._aacl._aaco._aacu._aacx._aad7._aade';
 
-    const imageUrl = await page.$eval(imgClass, (el) =>
-        el.getAttribute('src'));
-    const description = await page.$eval(descClass, (el) =>
-        el.innerText);
+    const imageUrl = await page.$eval(imgClass, (el) => el.getAttribute('src'));
+    const description = await page.$eval(descClass, (el) => el.innerText);
 
     await browser.close();
 
-    res.status(200).json({imageUrl, description});
+    res.status(200).json({ imageUrl, description });
   } catch (error) {
     console.error(error);
-    res.status(400).json({error: error.message});
+    res.status(400).json({ error: error.message });
   }
 }
