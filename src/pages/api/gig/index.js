b28985bf3e0181f282b058573e8d32a35da92196
Luke <luke.stephens@trintel.co.za>
2023-03-22 17:21:49 +0200

    add flag

--- src/pages/api/gig/index.js
+++ src/pages/api/gig/index.js
@@ -14,6 +14,7 @@ export default async function handler(req, res) {
       executablePath,
       args: edgeChromium.args,
       headless: true,
+      ignoreDefaultArgs: ['--disable-extensions']
     })
 
     const page = await browser.newPage();
