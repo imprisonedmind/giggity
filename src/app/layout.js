82b47a2b83f4f8a6c0f4208b5e3c4d2dde9850f6
Luke <luke.stephens@trintel.co.za>
2023-03-25 21:28:57 +0200

    test

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,7 @@
 import "./globals.css";
 import { QuickView } from "@/context/quickView";
 import Navbar from "@/components/navigation/navbar";
+import Head from "next/head";
 
 export const metadata = {
   title: "Giggity",
@@ -10,6 +11,10 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <Head>
+        <title></title>
+        <meta property={"og:image"} content={"/api/og"} />
+      </Head>
       <body className={"m-auto max-w-[1200px] bg-neutral-900 py-4 px-2 md:p-4"}>
         <QuickView>
           <Navbar />
