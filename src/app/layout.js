b05bca0e728284c22e30e3279995bffb520d803d
Luke <luke.stephens@trintel.co.za>
2023-03-25 22:49:04 +0200

    test 3

--- src/app/layout.js
+++ src/app/layout.js
@@ -11,6 +11,11 @@ import Head from "next/head";
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+        <title>Giggity</title>
+        <desc>Find local gigs near you!</desc>
+        <meta property={"og:image"} content={"/api/og"} />
+      </head>
       <body className={"m-auto max-w-[1200px] bg-neutral-900 py-4 px-2 md:p-4"}>
         <QuickView>
           <Navbar />
