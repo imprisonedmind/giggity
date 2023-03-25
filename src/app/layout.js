aebe4b67991d0ca7a3b0e42952cf2f98ceb6b227
Luke <luke.stephens@trintel.co.za>
2023-03-25 15:48:34 +0200

    mobile

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,23 +1,21 @@
-import './globals.css'
-import {QuickView} from "@/context/quickView";
+import "./globals.css";
+import { QuickView } from "@/context/quickView";
 import Navbar from "@/components/navigation/navbar";
 
 export const metadata = {
-  title: 'Giggity',
-  description: "Find local gis near you!"
-}
+  title: "Giggity",
+  description: "Find local gis near you!",
+};
 
-
-export default function RootLayout({children}) {
+export default function RootLayout({ children }) {
   return (
-      <html lang="en">
-      <body className={"bg-neutral-900 p-4 max-w-[1200px] m-auto"}>
-      <QuickView>
-        <Navbar/>
-        {children}
-      </QuickView>
-
+    <html lang="en">
+      <body className={"m-auto max-w-[1200px] bg-neutral-900 py-4 px-2 md:p-4"}>
+        <QuickView>
+          <Navbar />
+          {children}
+        </QuickView>
       </body>
-      </html>
-  )
+    </html>
+  );
 }
