ad8270823fd4398796ce4a5c600d90d7d92b8c6a
Luke <luke.stephens@trintel.co.za>
2023-03-25 15:46:59 +0200

    mobile

--- src/components/card/itemButton.js
+++ src/components/card/itemButton.js
@@ -13,7 +13,7 @@ export default function ItemButton({
       href={link}
       className={`${colour} ${textColour} ${hover}
       flex h-fit cursor-pointer flex-nowrap items-center gap-2 rounded-lg border 
-      border-[0.6px] p-2 text-center text-sm font-medium uppercase hover:shadow-md`}
+      border-[0.6px] px-2 py-4 text-center text-sm font-medium uppercase hover:shadow-md md:py-2`}
     >
       {icon && <div className={"h-4 w-4"}>{icon}</div>}
       <p className={"m-auto"}>{title}</p>
--- src/components/card/itemImage.js
+++ src/components/card/itemImage.js
@@ -1,9 +1,21 @@
 import Image from "next/image";
 
-export default function ItemImage({item}) {
-  return <div className={"relative h-72 overflow-hidden rounded-lg border" +
-      " border-1 border-neutral-700 shadow-2xl"}>
-    <Image src={item.image} alt={item.imgAlt} fill={"cover"}
-           className={"object-cover "} placeholder={"blur"} blurDataURL={item.image}/>
-  </div>
+export default function ItemImage({ item }) {
+  return (
+    <div
+      className={
+        "relative aspect-square h-[400px] overflow-hidden rounded-lg border md:h-72" +
+        " border-1 border-neutral-700 shadow-2xl"
+      }
+    >
+      <Image
+        src={item.image}
+        alt={item.imgAlt}
+        fill={true}
+        className={"object-cover "}
+        placeholder={"blur"}
+        blurDataURL={item.image}
+      />
+    </div>
+  );
 }
