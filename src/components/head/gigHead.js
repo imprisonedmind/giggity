943a70a3fca838c6d83ccf6c2e65e355219453f4
Luke <luke.stephens@trintel.co.za>
2023-03-26 12:20:01 +0200

    test some changes

--- src/components/head/gigHead.js
+++ src/components/head/gigHead.js
@@ -5,13 +5,11 @@ export default function GigHead({ item }) {
       <meta property="og:description" content={item.description} />
       <meta
         property="og:image"
-        content={`https://giggity-ruddy.vercel.app/api/gigImage?title=${item.title}&gigImg=${item.image}`}
-        // content={`https://giggity-ruddy.vercel.app/api/gigImage?title=${item.title}`}
+        content={`https://giggity-ruddy.vercel.app/api/gigImage?title="${item.title}"&gigImg="${item.image}"`}
       />
       <meta
         name="twitter:image"
-        content={`https://giggity-ruddy.vercel.app/api/gigImage?title=${item.title}&gigImg=${item.image}`}
-        // content={`https://giggity-ruddy.vercel.app/api/gigImage?title=${item.title}`}
+        content={`https://giggity-ruddy.vercel.app/api/gigImage?title="${item.title}"&gigImg="${item.image}"`}
       />
       <meta name="twitter:card" content="summary_large_image" />
     </head>
--- src/pages/api/gigImage/index.js
+++ src/pages/api/gigImage/index.js
@@ -125,21 +125,19 @@ export default async function (params) {
               />
             </svg>
           </div>
-          {gigImg && (
-            <div tw={"flex flex-col w-1/2 justify-center"}>
-              <div
-                tw={
-                  "flex h-[420px] w-[420px] rounded-3xl overflow-hidden border" +
-                  " border-[5px] border-neutral-600 shadow-lg m-auto"
-                }
-              >
-                <img src={gigImg} tw={"w-full h-full"} alt={title} />
-              </div>
-              <div tw={"flex text-neutral-500 text-[30px] text-center m-auto"}>
-                <p tw={"m-auto"}>{title}</p>
-              </div>
+          <div tw={"flex flex-col w-1/2 justify-center"}>
+            <div
+              tw={
+                "flex h-[420px] w-[420px] rounded-3xl overflow-hidden border" +
+                " border-[5px] border-neutral-600 shadow-lg m-auto"
+              }
+            >
+              <img src={gigImg} tw={"w-full h-full"} alt={title} />
             </div>
-          )}
+            <div tw={"flex text-neutral-500 text-[30px] text-center m-auto"}>
+              <p tw={"m-auto"}>{title}</p>
+            </div>
+          </div>
         </div>
       </div>
     ),
@@ -149,81 +147,3 @@ export default async function (params) {
     }
   );
 }
-//
-// import { NextRequest } from "next/server";
-// import { ImageResponse } from "@vercel/og";
-//
-// export const config = {
-//   runtime: "edge",
-// };
-//
-// export default function handler(req) {
-//   try {
-//     const { searchParams } = new URL(req.url);
-//
-//     // ?title=<title>
-//     const hasTitle = searchParams.has("title");
-//     const title = hasTitle
-//       ? searchParams.get("title")?.slice(0, 100)
-//       : "My default title";
-//
-//     return new ImageResponse(
-//       (
-//         <div
-//           style={{
-//             backgroundColor: "black",
-//             backgroundSize: "150px 150px",
-//             height: "100%",
-//             width: "100%",
-//             display: "flex",
-//             textAlign: "center",
-//             alignItems: "center",
-//             justifyContent: "center",
-//             flexDirection: "column",
-//             flexWrap: "nowrap",
-//           }}
-//         >
-//           <div
-//             style={{
-//               display: "flex",
-//               alignItems: "center",
-//               justifyContent: "center",
-//               justifyItems: "center",
-//             }}
-//           >
-//             <img
-//               alt="Vercel"
-//               height={200}
-//               src="data:image/svg+xml,%3Csvg width='116' height='100' fill='white' xmlns='http://www.w3.org/2000/svg'%3E%3Cpath d='M57.5 0L115 100H0L57.5 0z' /%3E%3C/svg%3E"
-//               style={{ margin: "0 30px" }}
-//               width={232}
-//             />
-//           </div>
-//           <div
-//             style={{
-//               fontSize: 60,
-//               fontStyle: "normal",
-//               letterSpacing: "-0.025em",
-//               color: "white",
-//               marginTop: 30,
-//               padding: "0 120px",
-//               lineHeight: 1.4,
-//               whiteSpace: "pre-wrap",
-//             }}
-//           >
-//             {title}
-//           </div>
-//         </div>
-//       ),
-//       {
-//         width: 1200,
-//         height: 630,
-//       }
-//     );
-//   } catch (e) {
-//     console.log(`${e.message}`);
-//     return new Response(`Failed to generate the image`, {
-//       status: 500,
-//     });
-//   }
-// }
