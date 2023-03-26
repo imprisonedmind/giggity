235820993a6318fe3ece73f77c210d144be256da
Luke <luke.stephens@trintel.co.za>
2023-03-26 11:30:47 +0200

    test 22

---
+++ src/components/head/gigHead.js
@@ -0,0 +1,19 @@
+export default function GigHead({ item }) {
+  return (
+    <head>
+      <meta property="og:title" content={item.title} />
+      <meta property="og:description" content={item.description} />
+      <meta
+        property="og:image"
+        content={`https://giggity-ruddy.vercel.app/api/gigImage?title=${item.title}&gigImg=${item.image}`}
+        // content={`https://giggity-ruddy.vercel.app/api/gigImage?title=${item.title}`}
+      />
+      <meta
+        name="twitter:image"
+        content={`https://giggity-ruddy.vercel.app/api/gigImage?title=${item.title}&gigImg=${item.image}`}
+        // content={`https://giggity-ruddy.vercel.app/api/gigImage?title=${item.title}`}
+      />
+      <meta name="twitter:card" content="summary_large_image" />
+    </head>
+  );
+}
