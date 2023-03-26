2bedae135bfe3092328392fc6cab90bceb12236c
Luke <luke.stephens@trintel.co.za>
2023-03-26 10:17:28 +0200

    test 16

--- src/app/gig/[uid]/page.js
+++ src/app/gig/[uid]/page.js
@@ -35,11 +35,13 @@ export default async function Gig({ params }) {
         <meta property="og:description" content={item.description} />
         <meta
           property="og:image"
-          content={`https://giggity-ruddy.vercel.app/api/gigImage?title=${item.title}&gigImg=${item.image}`}
+          // content={`https://giggity-ruddy.vercel.app/api/gigImage?title=${item.title}&gigImg=${item.image}`}
+          content={`https://giggity-ruddy.vercel.app/api/gigImage?title=${item.title}`}
         />
         <meta
           name="twitter:image"
-          content={`https://giggity-ruddy.vercel.app/api/gigImage?title=${item.title}&gigImg=${item.image}`}
+          // content={`https://giggity-ruddy.vercel.app/api/gigImage?title=${item.title}&gigImg=${item.image}`}
+          content={`https://giggity-ruddy.vercel.app/api/gigImage?title=${item.title}`}
         />
         <meta name="twitter:card" content="summary_large_image" />
       </head>
