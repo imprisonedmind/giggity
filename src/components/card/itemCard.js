a9b70ef088f1a23e2f8c7bca6ffece3022975891
Luke <luke.stephens@trintel.co.za>
2023-04-17 15:44:36 +0200

    Propper full card

--- src/components/card/itemCard.js
+++ src/components/card/itemCard.js
@@ -11,9 +11,9 @@ export default function ItemCard({ item, full }) {
     <div
       key={item.id}
       className={`${
-        full ? "w-full" : "w-full max-w-[300px]"
-      } col-span-1 flex  flex-col justify-between rounded-xl
-         border border-neutral-700 bg-neutral-800 p-2`}
+        full ? "w-full " : "w-full max-w-[300px]"
+      } col-span-1 flex flex-col justify-between rounded-xl border
+         border-neutral-700 bg-neutral-800 p-2 md:max-w-[300px]`}
     >
       <ItemImage item={item} />
       <div
