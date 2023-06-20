a07c4d9dd8e5dc8b797ffbc9a81d5d031c60bad3
Luke <luke.stephens@trintel.co.za>
2023-06-20 13:29:42 +0200

    add scroll on gigswrapper

--- src/components/gigWrapper/gigsWrapper.js
+++ src/components/gigWrapper/gigsWrapper.js
@@ -2,7 +2,9 @@ import ItemCard from "@/components/card/itemCard";
 
 export default function GigsWrapper({ data, m }) {
   return (
-    <div className={`${m} flex h-full w-full flex-wrap gap-4 p-2 md:p-4`}>
+    <div
+      className={`${m} flex h-full w-full flex-wrap gap-4 overflow-y-scroll p-2 md:p-4`}
+    >
       {data.map((item) => (
         <ItemCard item={item} key={item.id} full={true} />
       ))}
