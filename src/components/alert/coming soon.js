9dd01a8fc5c1164178f498cd7c1b83d6d6ceb6d1
Luke <luke.stephens@trintel.co.za>
2023-06-20 13:32:03 +0200

    remove login option on mobile as users are not fully setup yet.

--- src/components/alert/coming soon.js
+++ src/components/alert/coming soon.js
@@ -9,19 +9,20 @@ export default function ComingSoon() {
     <div
       className={
         "rounded-lg border border-neutral-700 bg-neutral-800 p-2" +
-        " flex flex-col gap-4 text-neutral-500"
+        " flex flex-col gap-2 text-neutral-500"
       }
     >
       <p>Coming Soon!</p>
       <p className={"text-sm"}>
-        This area will be available soon, in the meantime please create an
-        account.
+        This area will be available soon, in the process of shipping user
+        accounts. This will allow you to Sign Up & Log In as well as favourite
+        gigs and edit your profile.
       </p>
-      <GreenButton
-        icon={<ArrowRightIcon className={"h-w w-4"} />}
-        title={"Sign Up"}
-        callBack={() => router.push("onboard/signup")}
-      />
+      {/*<GreenButton*/}
+      {/*  icon={<ArrowRightIcon className={"h-w w-4"} />}*/}
+      {/*  title={"Sign Up"}*/}
+      {/*  callBack={() => router.push("onboard/signup")}*/}
+      {/*/>*/}
     </div>
   );
 }
