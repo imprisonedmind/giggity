6897f6df98133a3014822145897af0ae2d337ec8
Luke <luke.stephens@trintel.co.za>
2023-06-20 12:18:45 +0200

    add this

--- src/components/tempReRoute.js
+++ src/components/tempReRoute.js
@@ -6,11 +6,12 @@ export default function TempReRoute() {
   const router = useRouter();
 
   useEffect(() => {
-    const isMobileDevice = window.innerWidth < 768; // Adjust the screen width breakpoint as needed
-
-    if (isMobileDevice) {
-      router.push("/app"); // Replace "/app" with the desired route for mobile devices
-    }
+    router.push("/app");
+    // const isMobileDevice = window.innerWidth < 768; // Adjust the screen width breakpoint as needed
+    //
+    // if (isMobileDevice) {
+    //   router.push("/app"); // Replace "/app" with the desired route for mobile devices
+    // }
   }, []);
 
   return <div></div>;
