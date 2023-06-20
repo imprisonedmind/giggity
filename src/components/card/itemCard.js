c78d55ac4bc682d8991f0c7c2a92ce63da6d2c40
Luke <luke.stephens@trintel.co.za>
2023-06-20 16:00:17 +0200

    small changes.

--- src/components/card/itemCard.js
+++ src/components/card/itemCard.js
@@ -14,7 +14,7 @@ export default function ItemCard({ item, full }) {
       className={`${
         full ? "w-full " : "w-fit max-w-[300px]"
       } col-span-1 flex cursor-pointer flex-col justify-between rounded-xl 
-        border border-neutral-700 bg-neutral-800 p-2`}
+        border border-neutral-700 bg-neutral-800 p-2 md:max-w-[300px]`}
     >
       <ItemImage item={item} />
       <div
