bf40c65f97d0d0480b330c2ef9e688dc8e27d3ed
Luke <luke.stephens@trintel.co.za>
2023-06-20 11:14:35 +0200

    fetch lib import

--- src/app/app/previous/page.js
+++ src/app/app/previous/page.js
@@ -1,5 +1,5 @@
 import GigsWrapper from "@/components/gigWrapper/gigsWrapper";
-import { getPastAll } from "../../../lib/dataFetching";
+import { getPastAll } from "/lib/dataFetching";
 
 export default async function Page() {
   let data = await getPastAll();
