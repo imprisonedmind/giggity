aa0feb51fb5cfcca77c80205390ff486ec145f0c
Luke <luke.stephens@trintel.co.za>
2023-06-23 11:00:56 +0200

    small fix to footer

--- src/components/landing/footer/landingFooter.js
+++ src/components/landing/footer/landingFooter.js
@@ -7,46 +7,54 @@ export default function LandingFooter() {
   return (
     <div
       className={
-        "flex w-full flex-wrap justify-between gap-8 border-t md:gap-0" +
-        " border-neutral-700 p-4 py-8 text-neutral-500 md:p-20 md:py-20"
+        " w-full border-t border-neutral-700 p-4 py-8 text-neutral-500 "
       }
     >
-      <div className={"pr-40 md:pr-0"}>
-        <LandingFooterElement logo={<Logo />}>
+      <div
+        className={
+          " mx-auto flex max-w-[1280px] flex-wrap justify-between gap-8 md:gap-0 md:p-20 md:py-20"
+        }
+      >
+        <div className={"pr-40 md:pr-0"}>
+          <LandingFooterElement logo={<Logo />}>
+            <LandingFooterItem
+              copyright={`© 2023 - ${copyRightDate.getFullYear()}`}
+              link={"/"}
+            />
+          </LandingFooterElement>
+        </div>
+        <LandingFooterElement title={"Technology"}>
+          <LandingFooterItem title={"NextJs"} link={"https://nextjs.org/"} />
+          <LandingFooterItem title={"Vercel"} link={"https://vercel.com/"} />
           <LandingFooterItem
-            copyright={`© 2023 - ${copyRightDate.getFullYear()}`}
-            link={"/"}
+            title={"Supabase"}
+            link={"https://supabase.com/"}
+          />
+          <LandingFooterItem
+            title={"Cloud Vision"}
+            link={"https://cloud.google.com/vision"}
+          />
+          <LandingFooterItem
+            title={"Open Api gpt-3"}
+            link={"https://openai.com/gpt-4"}
           />
         </LandingFooterElement>
+        <LandingFooterElement title={"Creator"}>
+          <LandingFooterItem
+            title={"Luke Stephens"}
+            link={"https://bento.me/lukey"}
+          />
+          <LandingFooterItem
+            title={"Buy me a coffee"}
+            link={"https://www.buymeacoffee.com/lukestephens"}
+          />
+        </LandingFooterElement>
+        <LandingFooterElement title={"Download"}>
+          <LandingFooterItem title={"Desktop"} link={"/"} />
+          <LandingFooterItem title={"Android"} link={"/"} />
+          <LandingFooterItem title={"IOS"} link={"/"} />
+        </LandingFooterElement>
       </div>
-      <LandingFooterElement title={"Technology"}>
-        <LandingFooterItem title={"NextJs"} link={"https://nextjs.org/"} />
-        <LandingFooterItem title={"Vercel"} link={"https://vercel.com/"} />
-        <LandingFooterItem title={"Supabase"} link={"https://supabase.com/"} />
-        <LandingFooterItem
-          title={"Cloud Vision"}
-          link={"https://cloud.google.com/vision"}
-        />
-        <LandingFooterItem
-          title={"Open Api gpt-3"}
-          link={"https://openai.com/gpt-4"}
-        />
-      </LandingFooterElement>
-      <LandingFooterElement title={"Creator"}>
-        <LandingFooterItem
-          title={"Luke Stephens"}
-          link={"https://bento.me/lukey"}
-        />
-        <LandingFooterItem
-          title={"Buy me a coffee"}
-          link={"https://www.buymeacoffee.com/lukestephens"}
-        />
-      </LandingFooterElement>
-      <LandingFooterElement title={"Download"}>
-        <LandingFooterItem title={"Desktop"} link={"/"} />
-        <LandingFooterItem title={"Android"} link={"/"} />
-        <LandingFooterItem title={"IOS"} link={"/"} />
-      </LandingFooterElement>
     </div>
   );
 }
