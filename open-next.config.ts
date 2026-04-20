2c174bc1cf1bf4216aa794e7a54ed015629fca43
Luke <luke.stephens@trintel.co.za>
2026-04-20 13:35:35 +0200

    cf

--- open-next.config.ts
+++ open-next.config.ts
@@ -1,3 +1,5 @@
 import { defineCloudflareConfig } from "@opennextjs/cloudflare";
 
-export default defineCloudflareConfig();
+export default defineCloudflareConfig({
+  buildCommand: "npm run build:next",
+});
